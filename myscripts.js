c6a3c747f8430879495b1180d37b452451740a21
bgwolfe <bwolfe53144@yahoo.com>
2024-07-27 18:45:08 -0500

    Added user interface to the game through Javascript commands

--- myscripts.js
+++ myscripts.js
@@ -1,66 +1,94 @@
+/* Reference to the html file to show scoring */
+const myScore = document.getElementById("myScore");
+const compScore = document.getElementById("compScore");
+
+/* Define human and computer score variables, and put them into the page starting at 0 */
+let humanScore = 0;
+let computerScore = 0;
+myScore.innerText = humanScore;
+compScore.innerText = computerScore;
+
+/* Use querySelector to edit CSS styles to the buttons */
+const rock = document.querySelector("#rock");
+rock.style.cssText = "border-radius: 30px; border: 2px solid gray; color: black; background-color: white; padding: 30px; background-color:gray; margin: 40px; font-size: 30px;";
+
+const paper= document.querySelector("#paper");
+paper.style.cssText = "height: 160px; width: 100px; border: 2px solid blue; color: black; background: repeating-linear-gradient(to bottom, white 0px, white 20px, blue 20px, blue 21px); margin: 40px; font-size: 30px;";
+
+const scissors = document.querySelector("#scissors");
+scissors.style.cssText = "color: black; padding: 30px; background-image: linear-gradient(to top left, green 50%, white 50%); margin: 40px; font-size: 30px;";
+
+/* Format the page and change some CSS styles using querySelectors */
+const titlePage = document.querySelector("#titlePage");
+titlePage.style.cssText = "text-align: center; font-weight: bold; font-size: 60px";
+
+const userChoice = document.querySelector("#userChoice");
+userChoice.style.cssText = "text-align: center";
+
+const scoreboard = document.querySelector(".scoreboard");
+scoreboard.style.cssText = "display: grid; grid-template-columns: 1fr 1fr; text-align: center; margin-top: 100px";
+
+const scoreboardName = document.querySelectorAll(".scoreboardName");
+scoreboardName.forEach((e) => {
+    e.style.cssText = "padding: 10px; text-align: center; font-weight: bold; font-size:30px";
+});
+
+const scoreboardScore = document.querySelectorAll(".scoreboardScore");
+scoreboardScore.forEach((e) => {
+    e.style.cssText = "padding: 10px; text-align: center; font-size: 2em";
+});
+
+const roundText = document.getElementById("roundText");
+roundText.style.cssText = "font-weight: bold; font-size: 25px; text-align: center";
+
+/* Set up variables for the computer and added a function that randomly picks one of the array items */
 const myArray = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let randomString = myArray[Math.floor(Math.random() * 3)];
-    console.log(randomString);
     return randomString;
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose rock, paper, or scissors", '');
-    choice = choice.toLowerCase();
-    if (choice !== 'paper' && choice !=='rock' && choice !== 'scissors') {
-        alert(`You didn't pick rock paper or scissors, you gave the computer a free win!`);
+/* add an event listener that makes a choice based on which button you click */
+var theChoice = document.querySelector("#userChoice");
+theChoice.addEventListener("click", makeChoice, false);
+
+/*function to play one round of rock, paper, scissors */
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        roundText.innerText = `Computer also chose ${computerChoice}, it's a tie.`;
+    } else if ((humanChoice === 'rock' && computerChoice === 'scissors') 
+        || (humanChoice === 'paper' && computerChoice === 'rock') 
+        || (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        humanScore = ++humanScore
+        roundText.innerText = `Computer chose ${computerChoice}, and ${humanChoice} beats ${computerChoice} so you win!!!`;
     } else {
-        console.log(choice);
-        return choice;
+        roundText.innerText = `Computer chose ${computerChoice}, and ${computerChoice} beats ${humanChoice} so you lose!`;
+        computerScore = ++computerScore
+        
     }
-}
-
-let humanScore = 0;
-let computerScore = 0;
-let rounds = 0;
-
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            alert(`Computer also chose ${computerChoice}, it's a tie.`);
-        } else if ((humanChoice === 'rock' && computerChoice === 'scissors') 
-            || (humanChoice === 'paper' && computerChoice === 'rock') 
-            || (humanChoice === 'scissors' && computerChoice === 'paper')) {
-            humanScore = ++humanScore
-            alert(`Computer chose ${computerChoice}, and ${humanChoice} beats ${computerChoice} so you win!!!`);
+    myScore.innerText = humanScore;
+    compScore.innerText = computerScore;
+/* Stops the game by disabling buttons when a player reaches 5 */
+    if (humanScore >= 5 || computerScore >= 5) {
+        document.getElementById("rock").disabled = true;
+        document.getElementById("paper").disabled = true;
+        document.getElementById("scissors").disabled = true;
+        if (humanScore > computerScore) {
+            roundText.innerText = `You win ${humanScore} to ${computerScore}!`;
         } else {
-            alert(`Computer chose ${computerChoice}, and ${computerChoice} beats ${humanChoice} so you lose!`);
-            computerScore = ++computerScore
+            roundText.innerText = `The computer wins ${computerScore}-${humanScore}.`;
         }
-        rounds = rounds++
+    }
+}
+
+/* Initiate a game of rock paper scissors when the user clicks a button, then updates the score */
+function makeChoice(e) {
+    if (e.target !== e.currentTarget) {
+    var humanSelection = e.target.id;
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
     }
 
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-    alert(`Score- You: ${humanScore}  Computer: ${computerScore}`);
+    e.stopPropagation();
 }
-
-for (i = 0; i < 5; ++i) {
-    playGame();
-}
-
-if (humanScore > computerScore) {
-    alert('You win!');
-} else if (computerScore > humanScore) {
-    alert('The computer wins!');
-} else {
-    alert(`It's a tie.`);
-}
-
-
-
-
-
-
-    
-
-
